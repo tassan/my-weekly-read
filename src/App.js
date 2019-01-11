3ef1cce835304efece127dd1aaae7b04587654c6
Tassan <tassanf@gmail.com>
2019-01-11 15:15:44 -0200

    Last read Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import LinkHandler from './components/links/LinkHandler/LinkHandler'
+import LinkHandler from './components/links/LinkHandler/LinkHandler';
+import LastRead from './components/reading-list/LastRead/LastRead';
 
 class App extends Component {
   render() {
@@ -9,7 +9,21 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-md-12">
+            <div className="jumbotron">
+              <h1>What are you gonna read today?</h1>
+              <p>I bet you have a bunch of links to saved that you didn't read, uh? Let's change that NOW!</p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
             <LinkHandler></LinkHandler>
+          </div>
+        </div>
+        <hr/>
+        <div className="row">
+          <div className="col-md-12">
+            <LastRead></LastRead>
           </div>
         </div>
       </div>
--- src/components/links/LinkHandler/LinkHandler.js
+++ src/components/links/LinkHandler/LinkHandler.js
@@ -3,9 +3,9 @@ import React from 'react';
 class LinkHandler extends React.Component {
     render() {
         return <div className="input-group mb-3">
-            <input type="url" className="form-control"/>
+            <input type="url" className="form-control" placeholder="Pass the url here "/>
                 <div className="input-group-append">
-                    <button className="btn btn-outline-secondary">Save <span role="img" aria-label="card file box">🗃</span></button>
+                    <button className="btn btn-outline-primary">Save <span role="img" aria-label="card file box">🗃</span></button>
                 </div>
         </div>
     }
